5b19a362f784f36ab15ada496e161a046b6de40f
FrancDev <114122165+FrancDev@users.noreply.github.com>
2023-02-08 02:45:40 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ Si es nuevo cliente
 Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...) */
 
 
+//Metodo
 Cliente Datos = new Cliente("Franc Lopez", 31125885, "calle 12 7b-73", "franc@gmail.com", "Si");
 Console.WriteLine("\n----Datos Del Cliente----\n");
 Console.WriteLine(" Nombre Completo: {0} \n Telefono: {1} \n Direccion: {2} \n Email: {3} \n Nuevo Cliente?: {4}"
                     , Datos.Nom, Datos.Tel, Datos.Dir, Datos.Email, Datos.Est);
 
+
+//Estructura
 public struct Cliente
 {
     public Cliente(String nom, float tel, String dir, String email, String est)
@@ -22,7 +25,6 @@ public struct Cliente
         Dir = dir;
         Email = email;
         Est = est;
-
     }
 
     public String Nom { get; }
